1ca9bf6213556e7c8aced5068df86695ff64766b
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-20 01:07:03 -0200

    add lesson27

---
+++ lesson27/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+    ifstream file;
+    file.exceptions (ifstream::failbit | ifstream::badbit);
+
+    try {
+        file.open ("file.txt");
+
+        while (!file.eof ()) {
+            cout << file.get ();
+        }
+    } catch (ifstream::failure e) {
+        cout << e.what () << endl;
+        cout << "Error opening file" << endl;
+        exit (1);
+    }
+    file.close ();
+    return 0;
+}
