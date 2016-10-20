2941164fd937fc54f6058995c071a70b20054a1b
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-19 23:17:23 -0200

    add lesson 19

--- lesson20/main.cpp
+++ lesson20/main.cpp
@@ -2,13 +2,7 @@
 
 using namespace std;
 
-template <typename T, typename U>
-T max (T arg1, U arg2) {
-    return (arg1 > arg2) ? arg1 : arg2;
-}
-
-
 int main () {
-    cout << "Max value is: " << max (35.55, 13) << endl;
+    cout << "Hello, world!" << endl;
     return 0;
 }
