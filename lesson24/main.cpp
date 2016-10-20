266aed02eccd6b0ef8dfb7bb9aa333185fe5451e
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-20 00:41:56 -0200

    add lesson24

---
+++ lesson24/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class DivideByZero : public runtime_error {
+    
+    public:
+        DivideByZero () : runtime_error ("Divide by zero exception") { }
+};
+
+
+double quotient (double numer, double denom) {
+    if (denom == 0)
+        throw DivideByZero ();
+    else
+        return numer / denom;
+}
+
+
+int main () {
+
+    double numer, denom, ration;
+
+    cout << "Enter a numerator: ";
+    cin >> numer;
+    cout << "Enter a denominator: ";
+    cin >> denom;
+
+    try {
+        ration = quotient (numer, denom);
+        cout << "Result is: " << ration << endl;
+    } catch (DivideByZero &except) {
+        cout << except.what () << endl;
+    }
+    return 0;
+}
