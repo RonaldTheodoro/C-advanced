531e903dfa26019c5398400b3dde7bb3935788cb
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-20 01:06:38 -0200

    remove DivideByZero class from lesson26

---
+++ lesson26/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+double quotient (double numer, double denom) {
+    if (denom == 0)
+        throw DivideByZero ();
+    else
+        return numer / denom;
+}
+
+
+int main () {
+
+    double numer, denom, ration;
+
+    cout << "Enter a numerator: ";
+    cin >> numer;
+    cout << "Enter a denominator: ";
+    cin >> denom;
+
+    try {
+        ration = quotient (numer, denom);
+        cout << "Result is: " << ration << endl;
+    } catch (...) {
+        cout << "Exception throw and caught" << endl;
+    }
+    return 0;
+}
