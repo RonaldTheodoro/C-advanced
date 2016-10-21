9852c525ce9e450a8069ea9acccee4c94711ab9d
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-10-20 22:17:26 -0200

    add lesson29

---
+++ lesson29/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    cout.put ('H');
+    cout.put ('e');
+    cout.put ('l');
+    cout.put ('l');
+    cout.put ('o');
+    cout.put (',');
+    cout.put (' ');
+    cout.put ('w');
+    cout.put ('o');
+    cout.put ('r');
+    cout.put ('l');
+    cout.put ('d');
+    cout.put ('!');
+    cout.put ('\n');
+    cout << "Hello, world!" << flush;
+    cout << "Hello, world!" << ends;
+    return 0;
+}
